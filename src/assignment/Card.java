f6f04ef97572433ce99e13076e485edc009b5843
John D'Angelo <qazzaq6@yahoo.com>
2019-04-08 18:13:00 -0500

    Card and Player Stubs
    Added basic method headers to comply with our domain diagram.  Though we should consider renaming some method to follow standard naming conventions.

--- src/assignment/Card.java
+++ src/assignment/Card.java
@@ -1,5 +1,39 @@
 package assignment;
 
 public class Card {
-
+	private int suit;
+	private int number;
+	public boolean facing;
+	
+	public Card() {
+		// ...
+	}
+	
+	public Card(int s, int n, boolean f) {
+		suit = s;
+		number = n;
+		facing = f;
+	}
+	
+	
+	// should be named 'flip'
+	public void Flip() {
+		// do stuff
+	}
+	
+	// the following three methods should be renamed as 'getSuit' or 'getNumber'
+	public int CheckSuit() {
+		
+		return suit;
+	}
+	// what is the purpose of passing a player here?
+	public int CheckSuit(Player p) {
+		
+		return suit;
+	}
+	
+	public int CheckNumber() {
+		return number;
+	}
+	
 }
--- src/assignment/Player.java
+++ src/assignment/Player.java
@@ -1,5 +1,27 @@
 package assignment;
 
 public class Player {
+	public String name = "";
+	// public Hand hand;
+	// private Deck drawPile;
+	// private Deck playPile;
 
+	public Player() {
+		// ...
+	}
+	
+	public Player (String n/*, Hand h, Deck dp, Deck pp*/) {
+		name = n;
+		// hand = h;
+		// drawPile = dp;
+		// playPile = pp;
+	}
+	
+	
+	// should be named 'moveCard'
+	public boolean MoveCard(Card c/*, cardZone from, cardZone to*/) {
+		
+		
+		return false;
+	}
 }
