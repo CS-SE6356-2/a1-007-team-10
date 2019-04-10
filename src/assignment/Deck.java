dd70fa6bff266caaaca45735bc27ac28851d0dd0
maiflys <mai_media@outlook.com>
2019-04-09 20:54:48 -0500

    cardZone and Deck class creation
    rudimentary version of classes created

---
+++ src/assignment/Deck.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Deck extends cardZone{
+	private Card topCard;
+	private Card bottomCard;
+	
+	public Card checkTop(Player player) {
+		return topCard;
+	}
+	private void shuffle() {
+	}
+	private void fill() {
+		
+	}
+	
+}
---
+++ src/assignment/cardZone.java
@@ -0,0 +1,17 @@
+package assignment;
+
+public class cardZone {
+	private Card[] cardList;
+	private boolean visible;
+	private int size;
+	
+	public boolean moveTo(cardZone zone) {
+		return;
+	}
+	public boolean moveCard(Card card, cardZone zone) {
+		return;
+	}
+	public CardList[] check() {
+		return;
+	}
+}
