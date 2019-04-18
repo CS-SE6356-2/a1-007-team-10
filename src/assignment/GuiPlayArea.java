d5add7e3e97cfa73f91702d7e637a8f866fcdd3e
Cole Flandorfer <colefland0r@gmail.com>
2019-04-18 04:14:19 -0500

    Fixed many of the imports
    fixed the importing of SWT for the gui. The SWTResourceManager is still causing problems however

--- src/assignment/GuiPlayArea.java
+++ src/assignment/GuiPlayArea.java
@@ -1,21 +1,19 @@
 package assignment;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.widgets.Table;
 
-public class guiTest extends Composite {
+public class GuiPlayArea extends Composite {
 
 	/**
 	 * Create the composite.
 	 * @param parent
 	 * @param style
 	 */
-	public guiTest(Composite parent, int style) {
+	public GuiPlayArea(Composite parent, int style) {
 		super(parent, SWT.BORDER);
 		
 		Label title = new Label(this, SWT.NONE);
@@ -24,7 +22,7 @@ public class guiTest extends Composite {
 		
 		Composite handArea = new Composite(this, SWT.BORDER);
 		handArea.setBackground(SWTResourceManager.getColor(204, 255, 255));
-		handArea.setBounds(36, 295, 570, 155);
+		handArea.setBounds(10, 335, 936, 191);
 		
 		Label lblHand = new Label(handArea, SWT.BORDER);
 		lblHand.setBounds(250, 0, 55, 15);
@@ -32,7 +30,7 @@ public class guiTest extends Composite {
 		
 		Label card1 = new Label(handArea, SWT.BORDER);
 		card1.setBounds(31, 29, 75, 89);
-		card1.setText("Card");
+		card1.setText("Card1");
 		
 		Label card2 = new Label(handArea, SWT.BORDER);
 		card2.setText("Card");
@@ -67,49 +65,48 @@ public class guiTest extends Composite {
 		choose4.setBounds(345, 124, 75, 25);
 		
 		Button choose5 = new Button(handArea, SWT.NONE);
-		choose5.setGrayed(true);
 		choose5.setText("Choose");
-		choose5.setBounds(460, 124, 75, 25);
+		choose5.setBounds(450, 124, 75, 25);
 		
 		Composite playArea = new Composite(this, SWT.NONE);
 		playArea.setBackground(SWTResourceManager.getColor(107, 142, 35));
-		playArea.setBounds(36, 96, 570, 193);
+		playArea.setBounds(10, 96, 936, 233);
 		
 		Label lblDeck = new Label(playArea, SWT.NONE);
-		lblDeck.setLocation(194, 30);
+		lblDeck.setLocation(387, 30);
 		lblDeck.setSize(55, 15);
 		lblDeck.setText("Deck");
 		
 		Label lblDiscardPile = new Label(playArea, SWT.NONE);
-		lblDiscardPile.setLocation(300, 27);
+		lblDiscardPile.setLocation(493, 27);
 		lblDiscardPile.setSize(76, 15);
 		lblDiscardPile.setText("Discard Pile");
 		
 		Label discardCount = new Label(playArea, SWT.NONE);
-		discardCount.setLocation(301, 10);
+		discardCount.setLocation(494, 10);
 		discardCount.setSize(55, 15);
 		discardCount.setText("6 Cards");
 		
 		Label deckCount = new Label(playArea, SWT.NONE);
-		deckCount.setLocation(194, 11);
+		deckCount.setLocation(387, 11);
 		deckCount.setSize(55, 15);
 		deckCount.setText("32 Cards");
 		
 		Button btnDraw = new Button(playArea, SWT.NONE);
-		btnDraw.setBounds(194, 158, 75, 25);
+		btnDraw.setBounds(387, 158, 75, 25);
 		btnDraw.setText("Draw");
 		
 		Label discard = new Label(playArea, SWT.BORDER);
 		discard.setText("Card");
-		discard.setBounds(301, 57, 75, 89);
+		discard.setBounds(494, 57, 75, 89);
 		
 		Label deck = new Label(playArea, SWT.BORDER);
 		deck.setText("Card");
-		deck.setBounds(194, 57, 75, 89);
+		deck.setBounds(387, 57, 75, 89);
 		
 		Composite scoreArea = new Composite(this, SWT.NONE);
 		scoreArea.setBackground(SWTResourceManager.getColor(102, 205, 170));
-		scoreArea.setBounds(71, 10, 535, 80);
+		scoreArea.setBounds(298, 10, 430, 80);
 		
 		Label lblPlayer1 = new Label(scoreArea, SWT.BORDER);
 		lblPlayer1.setBounds(10, 10, 55, 15);
