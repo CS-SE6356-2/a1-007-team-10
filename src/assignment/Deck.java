4ad27ba4cf1b72493d504f9102b2620e974de900
Cole Flandorfer <colefland0r@gmail.com>
2019-04-18 23:07:50 -0500

    Revert "Simplified the Hand and Deck class"
    This reverts commit 4e00497f4a8b5948caf94088c6a5504721854856.

--- src/assignment/Deck.java
+++ src/assignment/Deck.java
@@ -1,43 +1,51 @@
 package assignment;
 
-// This class should be used to only create a standard deck of cards
+import java.util.Random;
+
 public class Deck extends cardZone{
 	//fields
-	//private Card topCard;
-	//private Card bottomCard; // do we need this anymore?
+	private Card topCard;
+	private Card bottomCard; // do we need this anymore?
 
 	//constructor
-	public Deck(String label){
-		super(label);
-
-		for (int suit = 0; suit <= 3; suit++) {
-			for (int rank = 1; rank <= 13; rank++) {
-				addCard(new Card(rank, suit));
-			}
-		}
-		// topCard = cardList.get(0);
-		// bottomCard = cardList.get(size-1);
+	Deck(){
+		topCard = cardList.get(0);
+		bottomCard = cardList.get(size-1);
 	}
 
 	//methods
-	// cardZone can accomplish this method
-	// public Card checkTop(Player player) {
-	// 	return topCard;
-	// }
+	public Card checkTop(Player player) {
+		return topCard;
+	}
 
 	// a fisher yates shuffle
 	// needs a toString method to return back to crazy8s
-	// why do we need this shuffle method since theres already one in cardZone
-	// public void shuffle() {
-	// 	int index;
-	// 	Card temp;
-	//     Random random = new Random();
-	//     for (int i = cardList.size(); i > 0; i--)
-	//     {
-	//         index = random.nextInt(i + 1);
-	//         temp = cardList.get(index);
-	//         cardList.set(index, cardList.get(i));
-	//         cardList.set(i, temp);
-	//     }
-	// }
+	public void shuffle() {
+		int index;
+		Card temp;
+	    Random random = new Random();
+	    for (int i = cardList.size(); i > 0; i--)
+	    {
+	        index = random.nextInt(i + 1);
+	        temp = cardList.get(index);
+	        cardList.set(index, cardList.get(i));
+	        cardList.set(i, temp);
+	    }
+	}
+
+	// what is this again?
+	private void fill() {
+
+	}
+
+	//methods for connecting to discardPile and drawPile
+	public void deal(Hand discardPile, int i) {
+
+	}
+
+	public void dealAll(Hand drawPile) {
+
+
+	}
+
 }
--- src/assignment/cardZone.java
+++ src/assignment/cardZone.java
@@ -1,18 +1,17 @@
 package assignment;
 
-import java.util.Random;
 import java.util.ArrayList;
 
-public class cardZone{
+public class cardZone {
 	//fields
 	protected ArrayList<Card> cardList = new ArrayList<Card>();
 	public boolean visible; //make private and make getter/setter?
 	protected int size;
-
+	
 	//constructors
 	cardZone(){
 		size = 0;
-		visible = false;
+		visible = false;	
 	}
 	cardZone(int s){
 		size = s;
@@ -21,76 +20,20 @@ public class cardZone{
 		}
 		visible = false;
 	}
-
-	public String getLabel() {
-		return label;
-	}
-
-	public void addCard(Card next) {
-		cards.add(next);
-	}
-
-	//pops card at given index
-	public Card popCard(int i) {
-		return cards.remove(i);
-	}
-
-	//returns and removes last card
-	public Card popCard() {
-		int i = size() - 1;
-		return popCard(i);
-	}
-
-	public int size() {
-		return cards.size();
-	}
-
-	public boolean empty() {
-		return cards.size() == 0;
-	}
-
-	public void deal(cardZone cards, int n) {
-		for (int i = 0; i < n; i++) {
-			Card card = popCard(i);
-			cards.addCard(card);
-		}
-	}
-
-	public Card returnLast() {
-		int i = size() - 1;
-		return cards.get(i);
-	}
-
-	public void swapCards(int i, int j) {
-		Card temp = cards.get(i);
-		cards.set(i, cards.get(j));
-		cards.set(j, temp);
-	}
-
-	public void shuffle() {
-		Random random = new Random();
-		for (int i = size() - 1; i > 0; i--) {
-			int j = random.nextInt(i);
-			swapCards(i, j);
-		}
-	}
+	
 	
 	//methods
 	// what is this again?
 	public boolean moveTo(cardZone cz) {
 		return true;
 	}
-
+	
 	public boolean moveCard(Card c, cardZone cz) {
 		cz.cardList.add(c);
 		return true;
 	}
-
+	
 	public ArrayList<Card> checkCardZone() {
 		return cardList;
-	}
-
-	public String toString() {									//temp toString
-		return label + ": " + cards.toString();
 	}
 }
