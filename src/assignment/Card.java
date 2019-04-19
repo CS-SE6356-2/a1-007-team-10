0e76d40fdbb614fe79717d6de58935d29493d09d
Cole Flandorfer <colefland0r@gmail.com>
2019-04-18 21:28:09 -0500

    Update Card.java

--- src/assignment/Card.java
+++ src/assignment/Card.java
@@ -18,7 +18,7 @@ public class Card {
 		suit = s;
 		number = n;
 		facing = f;
-		//get image
+		//get Image from disk
 		cardImage = SWTResourceManager.getImage(Card.class, "/testImages/tempCard.png"); //The first parameter of this may need to change as I'm not sure exactly what it does
 	}
 	
