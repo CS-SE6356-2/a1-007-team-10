4a49d70f48c49a66de87d258a2caaf07349fe860
Steve Johnson <spj160030@utdallas.edu>
2019-04-18 23:03:58 -0500

    Updated player class with additional methods

--- src/assignment/Player.java
+++ src/assignment/Player.java
@@ -3,22 +3,23 @@ package assignment;
 public class Player {
 	public String name = "";
 	public Hand hand;
-	private Deck drawPile;
-	private cardZone playPile;
-	
-	
+	//these piles should to be in the main crazy8s class
+	//private Deck drawPile;
+	//private cardZone playPile;
+
+
 	public Player() {
 		System.out.println("Player constructor should be called with arguments.");
 	}
-	
-	public Player (String n, Hand h, Deck dp, cardZone pp) {
-		name = n;
-		hand = h;
-		drawPile = dp;
-		playPile = pp;
+
+	public Player (String n) {
+		this.name = n;
+		this.hand = new Hand(n);
+		//drawPile = dp;
+		//playPile = pp;
 	}
-	
-	
+
+
 	// replaced with draw and playCard
 	/*
 	public boolean MoveCard(Card c, cardZone from, cardZone to) {
@@ -27,38 +28,78 @@ public class Player {
 		return false;
 	}
 	*/
-	
+
 	public boolean playCard(Card c) {
-		
+
 		return false;
 	}
-	
+
 	public boolean drawCard() {
-		
+
 		return false;
 	}
-	
+
 	// Returns ... something?
-	public int size() {
-		return -1;
-	}
-	
+	// idk if this is needed
+	// public int size() {
+	// 	return -1;
+	// }
+
 	// Returns the Hand object associated with this player
 	public Hand getHand() {
 		return hand;
 	}
-	
+
 	// Returns the card the player decided to play
 	// Should interface with the GUI
 	public Card play() {
 		return null;
 	}
-	
+
 	// Returns the name of this player
 	public String getName() {
 		return name;
 	}
-	
+
+	//takes turn for player if playing against PC
+	// Should interface with the GUI
+	public Card play(Crazy8s game, Card prev) {
+			Card card = searchForMatch(prev);
+			if (card == null) {
+					card = drawTill(game, prev);
+			}
+			return card;
+	}
+
+	//searches player's hand for matching card
+	public Card searchForMatch(Card prev) {
+		return null;
+	}
+
+	//adds max 3 cards to hand if hand does not contain match
+	public Card drawTill(crazy8s game, Card prev) {
+		return null;
+	}
+
+	//compares cards to see if they match
+	public static boolean cardMatch(Card card1, Card card2) {
+		return false;
+	}
+
+	//this method will need to calculate score based on cards in hand
+	public int score() {
+		return 0;
+	}
+
+	public int getScore() {
+		return 0;
+	}
+
+	// returns hand to display
+	public void display() {
+		hand.display();
+	}
+
 	public String toString() {
 		return name + ": " + hand.toString();
 	}
