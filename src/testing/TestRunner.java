948fb16ff8b1ce88e8b82447c83dc2545108856d
maiflys <mai_media@outlook.com>
2019-04-30 15:31:35 -0500

    Update TestRunner.java

--- src/testing/TestRunner.java
+++ src/testing/TestRunner.java
@@ -3,12 +3,46 @@ package testing;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 
 class TestRunner {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	public static void main(String[] args) {
+		//Test cardZone
+		Result cardZoneResult = JUnitCore.runClasses(cardZoneTest.class);
+		for(Failure failure : cardZoneResult.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+		System.out.println(cardZoneResult.wasSuccessful());
+
+		//Test Crazy8s
+		Result Crazy8sResult = JUnitCore.runClasses(Crazy8sTest.class);
+		for(Failure failure : Crazy8sResult.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+		System.out.println(Crazy8sResult.wasSuccessful());
+
+
+		//Test Deck
+		Result DeckResult = JUnitCore.runClasses(DeckTest.class);
+		for(Failure failure : DeckResult.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+		System.out.println(DeckResult.wasSuccessful());
+		
+		//Test Player
+		Result PlayerResult = JUnitCore.runClasses(PlayerTest.class);
+		for(Failure failure : PlayerResult.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+		System.out.println(PlayerResult.wasSuccessful());
 	}
 
 }
