2b1333a3250fa371419a81b21144ea7014ddf371
Steve Johnson <spj160030@utdallas.edu>
2019-05-01 21:10:00 -0500

    Update Crazy8s.java
    Moved code from playsGame to main

--- src/base/Crazy8s.java
+++ src/base/Crazy8s.java
@@ -4,11 +4,11 @@ import cardGui.*;
 
 public class Crazy8s {
 	// Fields
-	private Deck drawPile;
+	private static Deck drawPile;
 	private cardZone playPile;
-	private ArrayList<Player> players;
-	
-	
+	private static ArrayList<Player> players;
+
+
 	// Constructor
 	public Crazy8s() {
 		// Initialize data objects used within the game
@@ -16,22 +16,22 @@ public class Crazy8s {
 		drawPile = new Deck("Draw Pile");
 		players = new ArrayList<Player>();
 	}
-	
-	
+
+
 	// Methods
 	public void addPlayer(String name) {
 		players.add(new Player(name, drawPile, playPile));
 	}
-	
+
 	public Player getPlayer(int i) {
 		return players.get(i);
 	}
-	
+
 	// Returns the index of the given player object in the internal player list
 	public int getPlayerIndex(Player player) {
 		return players.indexOf(player);
 	}
-	
+
 	// Returns true if any players hand is empty
 	public boolean isDone() {
 		for (int i = 0; i < players.size(); i++) {
@@ -39,34 +39,46 @@ public class Crazy8s {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	// Returns the player object that will have the turn after the player passed to the method
 	public Player nextPlayer(Player current) {
 		// If the current player is not the last one
 		if(getPlayerIndex(current) < players.size() - 1) {
 			return getPlayer(getPlayerIndex(current) + 1);
 		}
-		
+
 		// If the player is the last one
 		if(getPlayerIndex(current) == players.size()) {
 			return players.get(0);
 		}
-		
+
 		return null;
 	}
-	
-	// Plays the game
+
+/*	// Plays the game
 	public void playGame() {
 		GuiMaster master = new GuiMaster();
-		//master.gameStart();
-		
+
+		// Main game loop
+		while(!isDone()) {
+			//master.nextState();
+		}
+
+		// if another game is going to be played, master calls playGame again
+	}*/
+
+
+	// Main Method
+	public static void main(String[] args) {
+		GuiMaster master = new GuiMaster();
+		int playerCnt = master.getPlayerCount();
 
 		// Filling player decks
 		// if two players, each starts with 7 cards
-		if(master.getPlayerCount() == 2) {
+		if(playerCnt == 2) {
 			for(int i = 0; i < 2; i++) {
 				for(int j = 0; j < 7; j++) {
 					drawPile.draw(players.get(i).hand);
@@ -74,27 +86,17 @@ public class Crazy8s {
 			}
 		} // otherwise each starts with 5 cards
 		else {
-			for(int i = 0; i < master.getPlayerCount(); i++) {
+			for(int i = 0; i < playerCnt; i++) {
 				for(int j = 0; j < 5; j++) {
 					drawPile.draw(players.get(i).hand);
 				}
 			}
 		}
-		// Main game loop
-		while(!isDone()) {
-			//master.nextState();
-		}
-		
-		// if another game is going to be played, master calls playGame again
-	}
-	
-	
-	// Main Method
-	public static void main(String[] args) {
-		GuiMaster master = new GuiMaster();
+
+
 		//System.out.println(master.getPlayerCount()/* + ' ' + master.getNameList().get(0)*/);
 		//master.turnStart("nametest");
-		
+
 		//master.message("This is my test message This is my test message This is my test message This is my test message ");
 	}
 }
